9c79d011403b9bd60c37891dd33407146dff5747
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-01 18:18:44 +0530

    fix:logic_issue & add:comment

--- logic/extract_image_from_pdf.go
+++ logic/extract_image_from_pdf.go
@@ -2,7 +2,7 @@ package logic
 
 import (
 	"fmt"
-	"image/jpeg"
+	"image/png"
 	"log"
 	"os"
 	"path/filepath"
@@ -28,63 +28,72 @@ func Extract_image_from_pdf(pathFile string) {
 
 func Extract_image_from_pdf_unidoc(pathFile string) {
 	log.Println("Started Extracting Embedded Images Using Unidoc pkg....")
-	var out_dir string = "extract_images_unidoc"
-	if err := os.Mkdir(out_dir, 0755); err != nil {
+
+	var out_dir string = "extract_images_unidoc"    // output directory name
+	if err := os.Mkdir(out_dir, 0755); err != nil { // 755 owner can read ,write and execute will other can read
 		log.Printf("Failed to create directory %s and error is %v", out_dir, err)
 	}
 
-	doc, err := os.Open(pathFile)
+	doc, err := os.Open(pathFile) // open the pdf file
 	if err != nil {
 		log.Printf("Failed to open the file %s, error is %v", filepath.Base(pathFile), err)
 	}
-	defer doc.Close()
-	pdfReader, err := model.NewPdfReader(doc)
+
+	defer doc.Close() // close the file when function end
+
+	pdfReader, err := model.NewPdfReader(doc) // Initialize the pdf reader and read the open file
 	if err != nil {
 		log.Println("Error Initalization pdf reader ", err)
 	}
 
-	numPage, err := pdfReader.GetNumPages()
+	numPage, err := pdfReader.GetNumPages() // total no. of page in pdf
 	if err != nil {
 		log.Println("Error in getting ", err)
 	}
 	log.Println("Total no. of pages it contain", numPage)
 
-	for i := 1; i <= numPage; i++ {
-		page, err := pdfReader.GetPage(i)
-		if err != nil {
+	total := 0 // to count no of photo and used in creating a name
+
+	for i := 1; i <= numPage; i++ { // loop all the page to get the embedded image in pdf
+		page, err := pdfReader.GetPage(i) // get the page content of particular page in pdf file
+		if err != nil {                   // if error occur then continue to next page
 			log.Printf("Error in getting page %d no.", i)
 			continue
 		}
 
-		pextract, err := extractor.New(page)
+		pextract, err := extractor.New(page) // Initialize the extractor for that particular page
 		if err != nil {
 			log.Println("Error in Initializing extractor ", err)
 		}
 
-		pimage, err := pextract.ExtractPageImages(nil)
+		pimage, err := pextract.ExtractPageImages(nil) // extracting the embedded image from that page and return as a list of images content
 		if err != nil {
 			log.Println("Error in Extracting the Images ", err)
 		}
 
-		for idx, img := range pimage.Images {
-			gimg, err := img.Image.ToGoImage()
+		for _, img := range pimage.Images { // loop all the images
+			log.Printf("Started Extracting Image %d No.", total+1)
+			gimg, err := img.Image.ToGoImage() // it will give golang image structure type Image interface {ColorModel() color.Model Bounds() Rectangle At(x, y int) color.Color}
 			if err != nil {
 				log.Println("Failed to load the image ", err)
 			}
 			// log.Println(gimg)
-			filePath := filepath.Join(out_dir, fmt.Sprintf("page_%d.jpg", idx+1))
-			f, err := os.Create(filePath)
+			filePath := filepath.Join(out_dir, fmt.Sprintf("page_%d_.png", total+1)) // create a file path
+			total++
+			f, err := os.Create(filePath) // creating a file
 			if err != nil {
 				log.Println("Failed to create file:", err)
 				return
 			}
-			defer f.Close()
 
-			err = jpeg.Encode(f, gimg, nil)
+			err = png.Encode(f, gimg) // now encoding the image data on file
 			if err != nil {
 				log.Println("Failed to encode jpeg format:", err)
 			}
+
+			f.Close() // closing the file immeditately a to prevent from resource leaks
+
 		}
 	}
-	log.Println("Exxtracting successfull")
+	log.Println("PDF Extracting successfull")
 }
