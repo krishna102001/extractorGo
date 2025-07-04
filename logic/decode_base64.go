66bf2aaf9083c78ad47dbcb07f284a9dfb1b2411
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-04 14:37:46 +0530

    fix:return error

--- logic/decode_base64.go
+++ logic/decode_base64.go
@@ -2,27 +2,41 @@ package logic
 
 import (
 	"encoding/base64"
+	"errors"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-func DecodeBase64(st string) {
+func DecodeBase64(st string) error {
 	file_byte, err := base64.StdEncoding.DecodeString(st) // decoding the base64 string  into a file byte
 	if err != nil {
 		log.Printf("Error Occurred while decoding the string %v", err)
+		return errors.New("failed to decode")
 	}
 
+	var out_pdf_file string = "out_pdf_file"
+	if err := os.Mkdir(out_pdf_file, 0755); err != nil {
+		log.Printf("Failed to make the directory %v", err)
+		return errors.New("failed to create directory")
+	}
+
+	filePath := filepath.Join(out_pdf_file, "sample.pdf")
+
 	//creating a file
-	file, err := os.Create("sample.pdf")
+	file, err := os.Create(filePath)
 	if err != nil {
 		log.Printf("Failed to create a file %v", err)
+		return errors.New("failed to create a file")
 	}
 
 	len_byte, err := file.Write(file_byte) // write the byte in file
 	if err != nil {
 		log.Println("Failed to write the byte content in file ", err)
+		return errors.New("failed to write the byte content")
 	}
 
 	file.Close()
 	log.Println("Total length of byte written are ", len_byte)
+	return nil
 }
--- logic/extract_image_from_pdf.go
+++ logic/extract_image_from_pdf.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"errors"
 	"fmt"
 	"image/png"
 	"log"
@@ -26,17 +27,19 @@ func Extract_image_from_pdf(pathFile string) {
 	log.Println("Finished Extracting Embedded Images.......")
 }
 
-func Extract_image_from_pdf_unidoc(pathFile string) {
+func Extract_image_from_pdf_unidoc(pathFile string) error {
 	log.Println("Started Extracting Embedded Images Using Unidoc pkg....")
 
 	var out_dir string = "extract_images_unidoc"    // output directory name
 	if err := os.Mkdir(out_dir, 0755); err != nil { // 755 owner can read ,write and execute will other can read
 		log.Printf("Failed to create directory %s and error is %v", out_dir, err)
+		return errors.New("failed to create a directory")
 	}
 
 	doc, err := os.Open(pathFile) // open the pdf file
 	if err != nil {
 		log.Printf("Failed to open the file %s, error is %v", filepath.Base(pathFile), err)
+		return errors.New("failed to open the file")
 	}
 
 	defer doc.Close() // close the file when function end
@@ -44,11 +47,13 @@ func Extract_image_from_pdf_unidoc(pathFile string) {
 	pdfReader, err := model.NewPdfReader(doc) // Initialize the pdf reader and read the open file
 	if err != nil {
 		log.Println("Error Initalization pdf reader ", err)
+		return errors.New("error in initializing the pdf reader ")
 	}
 
 	numPage, err := pdfReader.GetNumPages() // total no. of page in pdf
 	if err != nil {
-		log.Println("Error in getting ", err)
+		log.Println("Error in getting total length of pdf page ", err)
+		return errors.New("error in getting total no. of pdf pages")
 	}
 	log.Println("Total no. of pages it contain", numPage)
 
@@ -64,11 +69,13 @@ func Extract_image_from_pdf_unidoc(pathFile string) {
 		pextract, err := extractor.New(page) // Initialize the extractor for that particular page
 		if err != nil {
 			log.Println("Error in Initializing extractor ", err)
+			return errors.New("error in initializing the extractor")
 		}
 
 		pimage, err := pextract.ExtractPageImages(nil) // extracting the embedded image from that page and return as a list of images content
 		if err != nil {
 			log.Println("Error in Extracting the Images ", err)
+			return errors.New("error in extracting the images")
 		}
 
 		for _, img := range pimage.Images { // loop all the images
@@ -76,6 +83,7 @@ func Extract_image_from_pdf_unidoc(pathFile string) {
 			gimg, err := img.Image.ToGoImage() // it will give golang image structure type Image interface {ColorModel() color.Model Bounds() Rectangle At(x, y int) color.Color}
 			if err != nil {
 				log.Println("Failed to load the image ", err)
+				return errors.New("failed to load the images")
 			}
 			// log.Println(gimg)
 			filePath := filepath.Join(out_dir, fmt.Sprintf("page_%d_.png", total+1)) // create a file path
@@ -83,12 +91,13 @@ func Extract_image_from_pdf_unidoc(pathFile string) {
 			f, err := os.Create(filePath) // creating a file
 			if err != nil {
 				log.Println("Failed to create file:", err)
-				return
+				return errors.New("failed to create a file")
 			}
 
 			err = png.Encode(f, gimg) // now encoding the image data on file
 			if err != nil {
-				log.Println("Failed to encode jpeg format:", err)
+				log.Println("Failed to encode in png format:", err)
+				return errors.New("failed to encode in png format ")
 			}
 
 			f.Close() // closing the file immeditately a to prevent from resource leaks
@@ -96,4 +105,5 @@ func Extract_image_from_pdf_unidoc(pathFile string) {
 		}
 	}
 	log.Println("PDF Extracting successfull")
+	return nil
 }
