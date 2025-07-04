06f1b5eec88283aaf91bb66059f22ca77036569b
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-04 14:38:54 +0530

    done:logic integration

--- routes/extract_route.go
+++ routes/extract_route.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/krishna102001/extract_image_from_pdf/logic"
 )
 
 func ExtractPDFImageRoutes(c *gin.Context) {
@@ -17,4 +18,16 @@ func ExtractPDFImageRoutes(c *gin.Context) {
 		return
 	}
 
+	//call the logic of decoding the string into base64
+	if err := logic.DecodeBase64(req.Input_Pdf_File); err != nil {
+		c.JSON(400, gin.H{"msg": "Check the base64 file"})
+		return
+	}
+
+	//call the logic to extract the pdf image from pdf file
+	if err := logic.Extract_image_from_pdf_unidoc("out_pdf_file/sample.pdf"); err != nil {
+		c.JSON(400, gin.H{"msg": "Failed to extract the pdf file"})
+	}
+
+	c.JSON(201, gin.H{"msg": "Image extracted successfully!!"})
 }
