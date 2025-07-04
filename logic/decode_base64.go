3f6115df5e1c15a4c0b6a33a83a5bdc3060f7c60
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-04 12:16:36 +0530

    add:comment & improve error handling

--- logic/decode_base64.go
+++ logic/decode_base64.go
@@ -7,15 +7,22 @@ import (
 )
 
 func DecodeBase64(st string) {
-	file_byte, err := base64.StdEncoding.DecodeString(st)
+	file_byte, err := base64.StdEncoding.DecodeString(st) // decoding the base64 string  into a file byte
 	if err != nil {
-		log.Println("err filebyte", err)
+		log.Printf("Error Occurred while decoding the string %v", err)
 	}
 
-	file, _ := os.Create("sample.pdf")
-	l, err := file.Write(file_byte)
+	//creating a file
+	file, err := os.Create("sample.pdf")
 	if err != nil {
-		log.Println("write", err)
+		log.Printf("Failed to create a file %v", err)
 	}
-	log.Println("total len : ", l)
+
+	len_byte, err := file.Write(file_byte) // write the byte in file
+	if err != nil {
+		log.Println("Failed to write the byte content in file ", err)
+	}
+
+	file.Close()
+	log.Println("Total length of byte written are ", len_byte)
 }
