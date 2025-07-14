0e13ff006069582606fc9b1f433562ab2a3a902c
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-14 15:45:53 +0530

    fix:retrieve env and resource-type

--- logic/cloudinary.go
+++ logic/cloudinary.go
@@ -4,18 +4,38 @@ import (
 	"context"
 	"errors"
 	"log"
+	"os"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+	"github.com/joho/godotenv"
 )
 
 type CloudinaryConfig struct {
 	Client *cloudinary.Cloudinary
 }
 
-func Cloudinarycredentials() *CloudinaryConfig {
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Failed to intitalized the env file")
+	}
+}
 
-	cld, err := cloudinary.NewFromParams("cloud_name", "cloud_api_key", "cloud_secret")
+func Cloudinarycredentials() *CloudinaryConfig {
+	cld_name := os.Getenv("CLOUDNIARY_NAME")
+	if cld_name == "" {
+		log.Fatalln("Failed to get the env of cloudinary-name")
+	}
+	cld_api_key := os.Getenv("CLOUDINARY_API_KEY")
+	if cld_api_key == "" {
+		log.Fatalln("Failed to get the env of cloudinary-api-key")
+	}
+	cld_secret := os.Getenv("CLOUDINARY_SECRET")
+	if cld_secret == "" {
+		log.Fatalln("Failed to get the env of cloudinary-secret")
+	}
+
+	cld, err := cloudinary.NewFromParams(cld_name, cld_api_key, cld_secret)
 	if err != nil {
 		log.Fatal("Failed to Initialize Cloudinary Client : ", err)
 	}
@@ -26,7 +46,8 @@ func Cloudinarycredentials() *CloudinaryConfig {
 
 func (cc *CloudinaryConfig) UploadFile(fileContent interface{}, folder string) (string, error) {
 	uploadParams := uploader.UploadParams{
-		Folder: folder,
+		Folder:       folder,
+		ResourceType: "raw",
 	}
 	uploadResult, err := cc.Client.Upload.Upload(context.Background(), fileContent, uploadParams)
 	if err != nil {
