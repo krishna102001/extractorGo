b70f578ee1126e585c1d1e2193cef37b514b2895
krishna102001 <krishnakantmaurya@icloud.com>
2025-07-15 15:22:05 +0530

    fix:generate_unique&return cloudinary_url as response

--- routes/extract_route.go
+++ routes/extract_route.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/krishna102001/extract_image_from_pdf/logic"
 )
 
@@ -37,8 +40,15 @@ func ExtractPDFImageRoutes(c *gin.Context) {
 			c.JSON(400, gin.H{"msg": "Provide either base64 or file input"})
 			return
 		}
+		//generating unique name
+		uniqueId, err := uuid.NewRandom()
+		if err != nil {
+			log.Println("failed to generate the uuid ", err)
+			c.JSON(500, gin.H{"msg": "failed to generate unique name"})
+			return
+		}
 
-		savePath := "./out_pdf_file/" + file.Filename
+		savePath := fmt.Sprintf("./out_pdf_file/ %v_%s", uniqueId, file.Filename)
 		if err := c.SaveUploadedFile(file, savePath); err != nil {
 			log.Println("Failed to save uploaded file:", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to save file"})
@@ -50,10 +60,16 @@ func ExtractPDFImageRoutes(c *gin.Context) {
 		return
 	}
 
+	var upld_url string
+	var err error
 	//call the logic to extract the pdf image from pdf file
-	if err := logic.Extract_image_from_pdf_unidoc(pdf_addr); err != nil {
+	if upld_url, err = logic.Extract_image_from_pdf_unidoc(pdf_addr); err != nil {
 		c.JSON(400, gin.H{"msg": "Failed to extract the pdf file"})
+		return
 	}
-
-	c.JSON(201, gin.H{"msg": "Image extracted successfully!!"})
+	err = os.Remove(pdf_addr)
+	if err != nil {
+		log.Printf("Failed to remove the file %s from disk after upload error is %v ", pdf_addr, err)
+	}
+	c.JSON(201, gin.H{"url": upld_url})
 }
